22c58b1114137c52e0e2664458faaa57a5100964
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2020-10-06 10:53:30 +0530

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -1,58 +1,26 @@
 import java.util.Date;
-import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-class Lab1SupportCallable implements Callable<String>
-{
-@Override
-	public String call() throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("\n\n in call for " + Thread.currentThread().getName());
-		return "Hello"+new Date();
-	}	
-}
-class Lab1Support implements Runnable
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+class Lab2Support implements Runnable
 {
 @Override
 public void run() {
 	System.out.println("\n\n");
 	for (int i = 1;i<10;i++){
-		System.out.println("For - " + i +  " in thread " + Thread.currentThread().getName());
-		try { Thread.sleep(10); 	} catch (InterruptedException e) { 		e.printStackTrace(); 		}
-
-//		if (i % 5 ==0)
-//               		int	k = 5/0;
-		
+		System.out.println("For - " + i +  " in thread " + Thread.currentThread().getName() + ", " + new Date());
+		try { Thread.sleep(200); 	} catch (InterruptedException e) { 		e.printStackTrace(); 		}
 	}
 }	
 }
-public class Lab1 {
 
-	
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		System.out.println("Press a number to continue");
-		Scanner scanner = new Scanner(System.in);
-		scanner.nextInt();
-	//	ExecutorService service = Executors.newSingleThreadExecutor();
-	//	ExecutorService service = Executors.newFixedThreadPool(2);
-		ExecutorService service = Executors.newCachedThreadPool();
-		System.out.println("after Thread pool creation");
-		service.execute(new Lab1Support());
-
-		service.execute(new Lab1Support());
-		service.execute(new Lab1Support());
-		service.execute(new Lab1Support());
-		service.execute(new Lab1Support());
-
-		service.execute(new Lab1Support());
-
-		/*	service.execute(new Lab1Support());
-		Future<String> future = service.submit(new Lab1SupportCallable());
-		System.out.println("after future");
-		System.out.println("call returned " + future.get());
-		*/service.shutdown();
-	}
+public class Lab2 {
+public static void main(String[] args) {
+	// ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+	 ScheduledExecutorService ses = Executors.newScheduledThreadPool(3);
+	 System.out.println("In main " + new Date());
+//	 ses.schedule(new Lab2Support(),10,TimeUnit.SECONDS);
+	ses.scheduleAtFixedRate(new Lab2Support(),5, 1,TimeUnit.SECONDS);
+	// ses.scheduleWithFixedDelay(new Lab2Support(),5, 5,TimeUnit.SECONDS);
+}
 }
