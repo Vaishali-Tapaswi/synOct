0fe700891ef7e68472432b2647cc374b135b406c
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2020-10-06 16:28:56 +0530

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -1,57 +1,75 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveAction;
 
-class Lab1Support extends Thread
-{
-	private String str;
-	private CountDownLatch latch;
-	
-public Lab1Support(String str, CountDownLatch latch) {
+class MyRecursiveAction extends RecursiveAction {
+
+    private List<Integer> inputlist ;
+
+
+    public MyRecursiveAction(List<Integer> inputlist) {
 		super();
-		this.str = str;
-		this.latch = latch;
+		this.inputlist = inputlist;
+		System.out.println("MyRecursiveAction Constructor " + inputlist.size());
 	}
 
-@Override
-public void run() {
-	for (int i = 0;i<10 ;i++){
-		System.out.print(str);
-		try { Thread.sleep((int)(Math.random()*1000)); 	} catch (InterruptedException e) { 		e.printStackTrace(); 		}
-	}
-	System.out.println("Currentn latch status  = " + latch.getCount());
-	latch.countDown();
-}	
+    private List<MyRecursiveAction> createSubtasks(List<Integer> list) {
+        List<MyRecursiveAction> subtasks =
+            new ArrayList<MyRecursiveAction>();
+        
+        MyRecursiveAction subtask1 = new MyRecursiveAction(list.subList(0, list.size()/2));
+        MyRecursiveAction subtask2 = new MyRecursiveAction(list.subList(list.size()/2, list.size()));
+
+        subtasks.add(subtask1);
+        subtasks.add(subtask2);
+
+        return subtasks;
+    }
+
+    
+	@Override
+    protected void compute() {
+
+        //if work is above threshold, break tasks up into smaller tasks
+		if (this.inputlist.size() > 20)
+		{
+			List<MyRecursiveAction> subtasks = new ArrayList<MyRecursiveAction>(); 
+					subtasks.addAll(createSubtasks(inputlist));
+			 for(RecursiveAction subtask : subtasks){
+				 System.out.println("Forking !!!");
+				 try { Thread.sleep(100); 	} catch (InterruptedException e) { 		e.printStackTrace(); 		}
+	                subtask.fork();
+	            }
+	  } else {
+		  for (Integer i : inputlist){
+			  try { Thread.sleep(100); 	} catch (InterruptedException e) { 		e.printStackTrace(); 		}
+
+			  if (i < 999 && i > 100)
+				  	System.out.println(i + "  filtered by " + Thread.currentThread().getName());
+		  }
+        }
+    }
 }
+public class ForkJoinDemo {
 
-public class Lab1_latch {
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Press a number to continue");
+		Scanner scanner = new Scanner(System.in);
+		scanner.nextInt();
 	
-public static void main(String[] args) throws InterruptedException {
-	System.out.println("Press a number to continue");
-	Scanner scanner = new Scanner(System.in);
-	//scanner.nextInt();
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i< 4000;i++)
+		{
+			list.add((int)(Math.random()*1000));
+		}
 	
-	CountDownLatch latch = new CountDownLatch(3);
-	Lab1Support t1 = new Lab1Support("A", latch);
-	t1.setName("AThread");
-	Lab1Support t2 = new Lab1Support("B",latch);
-	t2.setName("BThread");
-	Lab1Support t3 = new Lab1Support("C",latch);
-	t3.setName("CThread");
-	t1.start();
-	t2.start();
-	t3.start();
-	System.out.println(" before await");
-	latch.await();
-	System.out.println("all three threads  completed execution");
-/*	try {
-		t1.join();
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		ForkJoinPool pool = new ForkJoinPool(4);
+		pool.invoke(new MyRecursiveAction(list));
 	}
-	System.out.println("end of  main  after completing t1 ");
-	*/
-}
-}
 
+}
