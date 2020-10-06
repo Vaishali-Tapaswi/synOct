b2b290f930ddc2c8a7649e58286d526d90ad0f41
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2020-10-06 15:05:17 +0530

    Update CB.java

--- CB.java
+++ CB.java
@@ -1,13 +1,22 @@
+import java.util.concurrent.CyclicBarrier;
+
 class Lab5Helper extends Thread{
+	private CyclicBarrier barrier ;
 	
-	
+	public Lab5Helper(CyclicBarrier barrier) {
+		super();
+		this.barrier = barrier;
+	}
+
 	@Override
 	public void run() {
 		  try{
 			  Thread.sleep((int)(Math.random()*1000));
-			  System.out.println("Platering phase for " + Thread.currentThread().getName());
+			  System.out.println("Plastering phase for " + Thread.currentThread().getName());
+			  barrier.await();
 			  Thread.sleep((int)(Math.random()*1000));
 			  System.out.println("Colouring phase for " + Thread.currentThread().getName());
+			  barrier.await();
 			  Thread.sleep((int)(Math.random()*1000));
 			  System.out.println("Furniture phase for " + Thread.currentThread().getName());
 			  
@@ -19,10 +28,11 @@ class Lab5Helper extends Thread{
 	
 }
 public class Lab5CyclicBarrier {
-
+	
 	public static void main(String[] args) {
+		CyclicBarrier barrier = new CyclicBarrier(3);
 for(int i = 1; i<=3;i++){
-	Lab5Helper t1 = new Lab5Helper();
+	Lab5Helper t1 = new Lab5Helper(barrier);
 	t1.setName("Flat"+i);
 	t1.start();
 }
