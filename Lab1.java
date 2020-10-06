8a29b6f6c7658a0a34361202def4a27a58b11fed
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2020-10-06 10:28:19 +0530

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -21,6 +21,8 @@ public void run() {
 	System.out.println("\n\n");
 	for (int i = 1;i<10;i++){
 		System.out.println("For - " + i +  " in thread " + Thread.currentThread().getName());
+		try { Thread.sleep(10); 	} catch (InterruptedException e) { 		e.printStackTrace(); 		}
+
 //		if (i % 5 ==0)
 //               		int	k = 5/0;
 		
@@ -34,13 +36,23 @@ public class Lab1 {
 		System.out.println("Press a number to continue");
 		Scanner scanner = new Scanner(System.in);
 		scanner.nextInt();
-		ExecutorService service = Executors.newSingleThreadExecutor();
+	//	ExecutorService service = Executors.newSingleThreadExecutor();
+	//	ExecutorService service = Executors.newFixedThreadPool(2);
+		ExecutorService service = Executors.newCachedThreadPool();
+		System.out.println("after Thread pool creation");
+		service.execute(new Lab1Support());
+
 		service.execute(new Lab1Support());
 		service.execute(new Lab1Support());
 		service.execute(new Lab1Support());
+		service.execute(new Lab1Support());
+
+		service.execute(new Lab1Support());
+
+		/*	service.execute(new Lab1Support());
 		Future<String> future = service.submit(new Lab1SupportCallable());
 		System.out.println("after future");
 		System.out.println("call returned " + future.get());
-		service.shutdown();
+		*/service.shutdown();
 	}
 }
